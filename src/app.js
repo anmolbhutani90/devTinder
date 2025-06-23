bf02783db0e24680474a6cefa9aa80067d17a73d
Anmol Bhutani <anmol.bhutani@bold.com>
2025-06-23 13:34:49 +0530

    exploring get,post,delete routes

--- src/app.js
+++ src/app.js
@@ -2,13 +2,23 @@ const express = require('express');
 const app = express();
 
 
-app.get('/hello',(req,res)=>{
+app.use('/hello',(req,res)=>{
     res.send('Hello from the hello page');
 })
-app.get('/',(req,res)=>{
+
+
+app.get('/user',(req,res)=>{
+    res.send('Get calls for users')
+})
+app.post('/user',(req,res)=>{
+    res.send('Created user')
+})
+app.delete('/user',(req,res)=>{
+    res.send('Deleted user')
+})
+app.use('/',(req,res)=>{
     res.send('Hello from the dashboard');
 })
-
 
 app.listen(3000,()=>{
     console.log('server is running at port 3000');
